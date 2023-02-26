12047c763bd11677c9af1f7fe028bb041de9faba
Mahesh Warule <mahesh.warule.59@gmail.com>
2023-02-26 12:20:56 +0530

    staged changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { MessageService } from 'primeng/api';
+import {ToastModule} from 'primeng/toast';
+import { ErrorInterceptor } from './common/helpers/error.interceptor';
+import { NotFoundComponent } from './common/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthenticationModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    DashboardModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
